6b3e9421e8a42b99f9edff18e83431834e11ee37
Ackerley Tng <ackerleytng@gmail.com>
2018-08-25 14:07:39 +0800

    Modify main.js to run different functions

--- main.js
+++ main.js
@@ -3,11 +3,31 @@
 const rewire = require('rewire')
 const webtask = rewire('./webtask.js')
 
-const goGetSsb = webtask.__get__('goGetSsb')
-const parsePage = webtask.__get__('parsePage')
-const buildSummary = webtask.__get__('buildSummary')
+const testCurrentSsb = function () {
+  const goGetSsb = webtask.__get__('goGetSsb')
+  const parsePage = webtask.__get__('parsePage')
+  const buildSummary = webtask.__get__('buildSummary')
 
-goGetSsb()
-  .then(parsePage)
-  .then(buildSummary)
-  .then(s => console.log(s))
+  console.log('testCurrentSsb =======')
+  goGetSsb()
+    .then(parsePage)
+    .then(buildSummary)
+    .then(s => console.log(s))
+}
+
+const testFetchPast = function () {
+  const goGetSsb = webtask.__get__('goGetSsb')
+
+  console.log('testFetchPast ========')
+}
+
+if (process.argv.length == 2) {
+  testCurrentSsb()
+  testFetchPast()
+} else if (process.argv.length == 3) {
+  if ("--current".startsWith(process.argv[2])) {
+    testCurrentSsb()
+  } else if ("--past".startsWith(process.argv[2])) {
+    testFetchPast()
+  }
+}
